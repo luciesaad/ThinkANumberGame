86928ace10da4a57002d4fb5162d96caa900209e
Lars Strömberg <stromberg.lars@gmail.com>
2020-01-23 13:38:28 +0100

    Fixed non numeric values

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Logic logic = new Logic();
-        int guess;
+        int guess = -1;
 
         Scanner scan = new Scanner(System.in);
 
         int count = 0;
         logic.startNumbArray();
         logic.genArrayList();
-        do{
+        do {
             System.out.println("Write in number between 0 and 50: ");
-            guess = scan.nextInt();
+            boolean isNextInt = scan.hasNextInt();
+            if (isNextInt) {
+                guess = scan.nextInt();
+            }
+            scan.next();
+
 
             System.out.println(logic.isWinner(guess));
             System.out.println(logic.higher(guess));
             System.out.println(logic.lower(guess));
 
-          for(int i = 0; i < logic.getArrayList().size(); i++){
-              if(logic.getArrayList().get(i).equals(guess)){
-                  System.out.println("You hit a mine");
-                  count++;
-                  break;
-              }
-          }
+            for (int i = 0; i < logic.getArrayList().size(); i++) {
+                if (logic.getArrayList().get(i).equals(guess)) {
+                    System.out.println("You hit a mine");
+                    count++;
+                    break;
+                }
+            }
 
-            if(logic.isWinner((guess)).equals("You won!")){
+            if (logic.isWinner((guess)).equals("You won!")) {
                 break;
             }
             //TA BORT!!!!
             System.out.println(logic.getArrayList());
             count++;
 
-        }while(count < 10);
+        } while (count < 10);
         scan.close();
-
-
 
 
     }
